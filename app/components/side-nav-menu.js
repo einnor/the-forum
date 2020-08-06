dcbe9cda0971c0a219367336fc810ce41b51c6c8
Agathe Badia <ab@honeypot.io>
2020-08-06 17:39:36 +0200

    Bring back the categories of posts

--- app/components/side-nav-menu.js
+++ app/components/side-nav-menu.js
@@ -3,11 +3,11 @@ import { A } from '@ember/array';
 
 export default Component.extend({
  menuItems: A([
-   { name: 'news', numberOfPosts: 64 },
-  //  { name: 'works', numberOfPosts: 100 },
-  //  { name: 'culture', numberOfPosts: 23 },
-  //  { name: 'general', numberOfPosts: 200 },
-  //  { name: 'projects', numberOfPosts: 64 },
-  //  { name: 'support', numberOfPosts: 64 },
+  { name: 'news', numberOfPosts: 64 },
+  { name: 'works', numberOfPosts: 100 },
+  { name: 'culture', numberOfPosts: 23 },
+  { name: 'general', numberOfPosts: 200 },
+  { name: 'projects', numberOfPosts: 64 },
+  { name: 'support', numberOfPosts: 64 },
  ]),
 })
