b6807422d6a5162d8d40cc9012a0b4cc33d48f0b
Agathe Badia <ab@honeypot.io>
2020-08-06 18:12:28 +0200

    Wip sortby post.id

--- app/components/main-screen.js
+++ app/components/main-screen.js
@@ -1,4 +1,22 @@
 import Component from '@ember/component';
+import { A } from '@ember/array';
+
+let mapper = (model) => {
+  let { attributes, type } = model;
+  return { type, ...attributes };
+},
 
 export default Component.extend({
+  orderBy: A([
+    { icon: 'expand_less' },
+    { icon: 'expand_more' }
+  ]),
+
+  actions: {
+    orderByAsc() {
+      let sortAsc = (prev, next) => prev.id < next.id ? 1 :-1;
+      const posts = this.modelFor('posts');
+      posts.sort(sortAsc).map(mapper);
+    }
+  }
 });
