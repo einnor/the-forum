5c6769e959a6ff000ddc5f334789701b0af80128
Agathe Badia <ab@honeypot.io>
2020-08-06 21:42:20 +0200

    The array displaying the chevron icons needed to be placed in the main-screen-order

--- app/components/main-screen-order.js
+++ app/components/main-screen-order.js
@@ -1,7 +1,13 @@
 import Component from '@ember/component';
+import { A } from '@ember/array';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  orderActions: A([
+    { icon: 'expand_less' },
+    { icon: 'expand_more' }
+  ]),
+
   router: service(),
   actions: {
     orderBy(order) {
--- app/components/main-screen.js
+++ app/components/main-screen.js
@@ -2,8 +2,4 @@ import Component from '@ember/component';
 import { A } from '@ember/array';
 
 export default Component.extend({
-  orderActions: A([
-    { icon: 'expand_less' },
-    { icon: 'expand_more' }
-  ]),
 });
