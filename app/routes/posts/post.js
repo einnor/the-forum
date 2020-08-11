ddc8cc799005dbf2a20c997a3cb07f97c445d0a3
Agathe Badia <ab@honeypot.io>
2020-08-11 14:50:33 +0200

    Wip fix posts service and display a list of articles

--- app/routes/posts/post.js
+++ app/routes/posts/post.js
@@ -1,9 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { reads } from '@ember/object/computed';
 
-export default class PostsPostRoute extends Route {
+export default Route.extend({
+  posts: service(),
+  postItems: reads('posts.list'),
   model(params) {
-    const posts = this.modelFor('posts');
-    const post = posts.find(post => post.id === params.id);
+    const post = this.postItems.find(post => post.id === params.id);
     return post;
   }
-}
+})
--- app/services/posts.js
+++ app/services/posts.js
@@ -1,82 +1,86 @@
 import Service from '@ember/service';
 
 export default Service.extend({
+  list: [
+    { id: '1', author: 'John Doe', authorImageUrl: 'https://place-hold.it/100x100/fff/666', title: 'Hammock beard marfa mixtape', numberOfReplies: 14, lastReply: '17 hours ago', lastReplyBy: 'Jane Doe', content: 'Hammock beard marfa mixtape, hoodie unicorn swag pabst 3 wolf moon. Farm-to-table gochujang etsy put a bird on it, affogato PBR&B snackwave keffiyeh chicharrones wolf normcore knausgaard gluten-free post-ironic disrupt. Air plant semiotics taxidermy, fanny pack portland put a bird on it mlkshk godard YOLO hashtag cardigan keytar street art schlitz echo park. Butcher gochujang copper mug tbh, vaporware microdosing slow-carb synth af gluten-free tumblr pug taiyaki cronut. Art party pinterest kogi austin, keytar unicorn cliche. Gochujang banh mi hot chicken chia poutine keffiyeh deep v activated charcoal occupy.' },
+    { id: '2', author: 'James Pierre', authorImageUrl: 'https://place-hold.it/100x100/fff/666', title: 'Lorem ipsum', numberOfReplies: 54, lastReply: '2 minutes ago', lastReplyBy: 'Sarah Blech', content: 'Hammock beard marfa mixtape, hoodie unicorn swag pabst 3 wolf moon. Farm-to-table gochujang etsy put a bird on it, affogato PBR&B snackwave keffiyeh chicharrones wolf normcore knausgaard gluten-free post-ironic disrupt. Air plant semiotics taxidermy, fanny pack portland put a bird on it mlkshk godard YOLO hashtag cardigan keytar street art schlitz echo park. Butcher gochujang copper mug tbh, vaporware microdosing slow-carb synth af gluten-free tumblr pug taiyaki cronut. Art party pinterest kogi austin, keytar unicorn cliche. Gochujang banh mi hot chicken chia poutine keffiyeh deep v activated charcoal occupy.' },
+  ],
 
-  async fetch() {
-    try {
-      const response = await axios({
-        method: 'get',
-        url: 'http://localhost:4000/api/posts',
-      });
+  // async fetch() {
+  //   try {
+  //     const response = await axios({
+  //       method: 'get',
+  //       url: 'http://localhost:4000/api/posts',
+  //     });
 
-      if (response.status === 200) {
-        const categories = response.data;
-        this.set('list', categories);
-      }
-      return response;
-    } catch (exception) {
-      // Handle exception
-    }
-  },
+  //     if (response.status === 200) {
+  //       const categories = response.data;
+  //       this.set('list', categories);
+  //     }
+  //     return response;
+  //   } catch (exception) {
+  //     // Handle exception
+  //   }
+  // },
 
-  async save(name) {
-    const payload = { id, title, content, author };
-    try {
-      const response = await axios({
-        method: 'post',
-        url: 'http://localhost:4000/api/posts',
-        data: payload,
-      });
+  // async save(name) {
+  //   const payload = { id, title, content, author };
+  //   try {
+  //     const response = await axios({
+  //       method: 'post',
+  //       url: 'http://localhost:4000/api/posts',
+  //       data: payload,
+  //     });
 
-      if (response.status === 201) {
-        const createdCategory = response.data;
-        const categories = this.list;
-        categories.push(createdCategory);
-        this.set('list', categories);
-      }
-      return response;
-    } catch (exception) {
-      // Handle exception
-    }
-  },
+  //     if (response.status === 201) {
+  //       const createdCategory = response.data;
+  //       const categories = this.list;
+  //       categories.push(createdCategory);
+  //       this.set('list', categories);
+  //     }
+  //     return response;
+  //   } catch (exception) {
+  //     // Handle exception
+  //   }
+  // },
 
-  async update(id, name) {
-    const payload = { name };
-    try {
-      const response = await axios({
-        method: 'put',
-        url: `http://localhost:4000/api/posts/${id}`,
-        data: payload,
-      });
+  // async update(id, name) {
+  //   const payload = { name };
+  //   try {
+  //     const response = await axios({
+  //       method: 'put',
+  //       url: `http://localhost:4000/api/posts/${id}`,
+  //       data: payload,
+  //     });
 
-      if (response.status === 201) {
-        const updatedCategory = response.data;
-        const categories = this.list;
-        const indexOfOldCategory = categories.findIndex(cat => cat.id === id);
-        categories[indexOfOldCategory] = updatedCategory;
-        this.set('list', categories);
-      }
-      return response;
-    } catch (exception) {
-      // Handle exception
-    }
-  },
+  //     if (response.status === 201) {
+  //       const updatedCategory = response.data;
+  //       const categories = this.list;
+  //       const indexOfOldCategory = categories.findIndex(cat => cat.id === id);
+  //       categories[indexOfOldCategory] = updatedCategory;
+  //       this.set('list', categories);
+  //     }
+  //     return response;
+  //   } catch (exception) {
+  //     // Handle exception
+  //   }
+  // },
 
-  async remove(id) {
-    try {
-      const response = await axios({
-        method: 'delete',
-        url: `http://localhost:4000/api/posts/${id}`,
-      });
+  // async remove(id) {
+  //   try {
+  //     const response = await axios({
+  //       method: 'delete',
+  //       url: `http://localhost:4000/api/posts/${id}`,
+  //     });
 
-      if (response.status === 201) {
-        const categories = this.list;
-        const updatedCategories = categories.filter(cat => cat.id !== id);
-        this.set('list', updatedCategories);
-      }
-      return response;
-    } catch (exception) {
-      // Handle exception
-    }
-  },
+  //     if (response.status === 201) {
+  //       const categories = this.list;
+  //       const updatedCategories = categories.filter(cat => cat.id !== id);
+  //       this.set('list', updatedCategories);
+  //     }
+  //     return response;
+  //   } catch (exception) {
+  //     // Handle exception
+  //   }
+  // },
 });
