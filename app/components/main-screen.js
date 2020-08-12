600d06f35e2af269f714cde60935ef6ad2594bce
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-12 10:35:04 +0300

    Resolved model and service issue as a result of refactor

--- app/components/main-screen.js
+++
@@ -1,8 +0,0 @@
-import Component from '@ember/component';
-import { reads } from '@ember/object/computed';
-import { inject as service } from '@ember/service';
-
-export default Component.extend({
-  posts: service(),
-  posts: reads('posts.articles')
-})
--- app/routes/posts.js
+++ app/routes/posts.js
@@ -4,8 +4,7 @@ import { inject as service } from '@ember/service';
 
 export default Route.extend({
   posts: service(),
-  postItems: computed('posts.list', function() {
-    console.log(this.posts.list);
-    return this.posts.list
-  }),
+  model() {
+    return this.posts.list;
+  }
 })
