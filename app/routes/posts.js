c50745d2400ea210b8b3142df618e597f5885843
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-24 19:56:23 +0300

    Transition to home if not logged in

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -2,7 +2,9 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { reads } from '@ember/object/computed';
 
+
 export default Route.extend({
+  session: service(),
   posts: service(),
   postItems: reads('posts.list'),
   queryParams: {
@@ -10,7 +12,14 @@ export default Route.extend({
       refreshModel: true,
     }
   },
+
+  async beforeModel(transition) {
+    if (!this.session.isAuthenticated) {
+      this.transitionTo('/');
+    }
+  },
+
   model(params) {
     return this.postItems;
   }
-})
+});
