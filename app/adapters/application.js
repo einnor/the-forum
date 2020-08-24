b7d838575b51055d8be6b2cf4c4bba8fa98109fd
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-24 19:24:21 +0300

    Fetch posts

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,13 +4,17 @@ import { inject as service } from '@ember/service';
 
 export default JSONAPIAdapter.extend({
   session: service(),
+  host: 'http://localhost:4000',
+  namespace: 'api',
+  headers: null,
 
-  headers: computed('session.data.authenticated.token', function() {
-    let headers = {};
+  init() {
+    this._super(...arguments);
     if (this.session.isAuthenticated) {
+      const token = this.get('session.data.authenticated.token');
+      let headers = {};
       headers['Authorization'] = `Bearer ${this.session.data.authenticated.token}`;
+      this.set('headers', headers);
     }
-
-    return headers;
-  }),
+  },
 });
--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -4,8 +4,9 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
   router: service(),
   queryParams: ['categoryId', 'order'],
-  categoryId: null,
+  categoryId: 'd1fc8d70-a033-4c5c-b8a9-8e2ff8bb3158',
   order: null,
+  posts: [],
 
 
   // TODO Dirty
@@ -14,10 +15,13 @@ export default Controller.extend({
     console.log('transitioned');
   },
 
-  init() {
+  async init() {
     this._super(...arguments);
     console.log(this.router.currentRoute);
     // this.posts.fetch(catgoryId);
+    const posts = await this.store.query('post', { categoryId: this.categoryId });
+    console.log(posts);
+    this.set('posts', posts);
   },
 
   didReceiveAttrs() {
