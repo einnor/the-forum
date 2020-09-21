e1b0848247cf83b58e07017776234269fd040389
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-09-21 12:49:05 +0300

    Make the post hoverable and clickable

--- app/components/posts.js
+++ app/components/posts.js
@@ -1,8 +1,14 @@
 import Component from '@ember/component';
 import { reads } from '@ember/object/computed';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
   router: service(),
   activePostId: reads('router.currentRoute.parent.params.id'),
+
+  @action
+  goToPostDetails(postId) {
+    this.router.transitionTo('posts.post.details', postId);
+  }
 })
